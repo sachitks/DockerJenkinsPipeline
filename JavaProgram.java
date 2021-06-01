15c8a2242c137322d32dc626d8cb464a2cedab7f
Ubuntu <sachit.saldanha@mindtree.com>
2021-06-01 18:11:14 +0000

    Second version of the Java program

--- JavaProgram.java
+++ JavaProgram.java
@@ -3,6 +3,7 @@ public class JavaProgram {
 public static void main(String args[]) {
 
 System.out.print("I am your Java Program. Thank you");
+System.out.print("Second version of the program");
 
 }
 
