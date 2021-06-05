c8c70bb1a646eab12d9cf5527d3fdb3c03d96069
Ubuntu <sachit.saldanha@mindtree.com>
2021-06-05 05:34:51 +0000

    version 5 of program

--- src/main/java/JavaProgram.java
+++ src/main/java/JavaProgram.java
@@ -3,7 +3,7 @@ public class JavaProgram {
 public static void main(String args[]) {
 
 System.out.print("I am your Java Program. Thank you\n");
-System.out.print("Fourth version of the program\n");
+System.out.print("Fifth version of the program\n");
 
 }
 
