dae15fdd64a65590a7bac4ae8899c6d3e7e6fe68
Ubuntu <sachit.saldanha@mindtree.com>
2021-06-06 06:01:22 +0000

    version 18 of the program

--- src/main/java/JavaProgram.java
+++ src/main/java/JavaProgram.java
@@ -3,7 +3,7 @@ public class JavaProgram {
 public static void main(String args[]) {
 
 System.out.print("I am your Java Program. Thank you\n");
-System.out.print("Seventeenth version of the program\n");
+System.out.print("Eighteenth version of the program\n");
 
 }
 
